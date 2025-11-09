f58794662e2897296223d4b15fdd5b3e3ede5739
Sivakumar <balusiva2312@gmail.com>
2025-11-09 14:18:59 +0530

    new changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.assisthealth.cloud';
+const BASE_URL = import.meta.env.VITE_API_URL ;
+
 
 // Create axios instance with default config
 const api = axios.create({
